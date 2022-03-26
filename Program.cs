ff93468a96f368994d56e6ab0c6ff8d42670de62
EllGuilherme <grds1000@gmail.com>
2022-03-25 21:15:48 -0300

    finalização da tarefa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int primeiron, segundon, soma;
+
+Console.WriteLine("Cálculo da soma entre dois números.\n");
+
+Console.WriteLine("digite seu primeiro numero: ");
+primeiron = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite seu segundo numero.: ");
+segundon =  Convert.ToInt32(Console.ReadLine());
+
+soma = primeiron + segundon;
+
+
+Console.WriteLine($"\nSoma: {soma}");
